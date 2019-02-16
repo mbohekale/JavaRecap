ebda972479ac6b3e828318d0706e3523e4c93185
mbohekale <mbohekale@yahoo.com>
2019-02-16 10:50:27 +0100

    Add files via upload

--- JavaBasic/JavaBasicRecap.java
+++ JavaBasic/JavaBasicRecap.java
@@ -1,46 +1,18 @@
 public class JavaBasicRecap{
 	public static void main(String[] args){
-		//using commandline
+		//using eclipse
 		System.out.println("Hello World");
-		System.out.print("How many arguments?:"+args.length+"\n"+args[0]+args[1]+args[2]+"\n");
 		
-		for(int i=0;i<args.length;i++){			
-		System.out.println("(index):"+i+":"+ args[i]);
-		}
-		
-		System.out.println("Reverse order of arguments");
-		for(int i=args.length-1;i>=0;i--){			
-		System.out.println("(index):"+i+":"+ args[i]);
-		}
-		
-		System.out.println("Sum of the numbers is:");
-		int a[] ={1,2,4};
-		int sum = 0;
-		for(int num:a){			
-		System.out.print(sum+=num);
-		}
-		
-		System.out.println();
-		
-		System.out.println("EVEN NUMBERS:");
-		
-		for(int i = 0; i<10;i++){
-			if(i%2==0){	
-				System.out.println("EvenNumber:"+i);
-			}
-		}
-		
-		System.out.println();
-		
-		System.out.println("ODD NUMBERS:");
-		
-		for(int i = 0; i<10;i++){
-			if(i%2!=0){	
-				System.out.println("EvenNumber:"+i);
-			}
-		}
-		
-		
-		
+
+		System.out.println("How many arguments:?"+args.length);		
+		 args =new String[4];
+		 args[0]="1";
+		 args[1]="2";
+			System.out.println("Hello\n"+args[0]
+					+"\n"+args[1]
+					+"\n"+args[2]
+					+"\n"+args[3]
+					+"\nHow many arguments:"+args.length);
+
 	}
 }
