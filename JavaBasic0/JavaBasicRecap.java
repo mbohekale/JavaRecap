72392037efa83421751af188a6b908374a868ab4
mbohekale <mbohekale@yahoo.com>
2019-02-16 10:56:04 +0100

    Add files via upload

---
+++ JavaBasic0/JavaBasicRecap.java
@@ -0,0 +1,46 @@
+public class JavaBasicRecap{
+	public static void main(String[] args){
+		//using eclipse
+		System.out.println("Hello World");
+		System.out.print("How many arguments?:"+args.length+"\n"+args[0]+args[1]+args[2]+"\n");
+		
+		for(int i=0;i<args.length;i++){			
+		System.out.println("(index):"+i+":"+ args[i]);
+		}
+		
+		System.out.println("Reverse order of arguments");
+		for(int i=args.length-1;i>=0;i--){			
+		System.out.println("(index):"+i+":"+ args[i]);
+		}
+		
+		System.out.println("Sum of the numbers is:");
+		int a[] ={1,2,4};
+		int sum = 0;
+		for(int num:a){			
+		System.out.print(sum+=num);
+		}
+		
+		System.out.println();
+		
+		System.out.println("EVEN NUMBERS:");
+		
+		for(int i = 0; i<10;i++){
+			if(i%2==0){	
+				System.out.println("EvenNumber:"+i);
+			}
+		}
+		
+		System.out.println();
+		
+		System.out.println("ODD NUMBERS:");
+		
+		for(int i = 0; i<10;i++){
+			if(i%2!=0){	
+				System.out.println("EvenNumber:"+i);
+			}
+		}
+		
+		
+		
+	}
+}
